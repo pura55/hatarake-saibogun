fc526b64cde4100127ee143a461c0e4d2b6d929e
kyo <>
2026-05-15 17:50:12 +0900

    ゴールしたら、リザルト画面が出るようにした

--- project/Renew Project/Assets/Scripts/ResultScrips/Goal Text.cs
+++ project/Renew Project/Assets/Scripts/ResultScrips/Goal Text.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 public class Goal : MonoBehaviour
 {
     public GameObject goalText;
+    public GameObject blackPanel;
+
+    public ResultManager resultManager;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // プレイヤーがゴールに触れたら
         if (collision.CompareTag("Player"))
         {
+            // 黒幕表示
+            blackPanel.SetActive(true);
+
             //テキストを表示
             goalText.SetActive(true);
+
+            // リザルト表示
+            resultManager.ShowResult();
         }
 
         // プレイヤーを停止
