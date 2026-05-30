a93152c2913a0f3e08aace4cfb1a068dc4c9a0a1
ChibaYuuka <y.c.nekomura615@gmail.com>
2026-05-30 17:04:32 +0900

    スキルボタンに画像を設定
    SkillUnlock.cs追加
    Buttonbright.cs編集

--- project/Renew Project/Assets/Scripts/ButtonScrips/Buttonbright.cs
+++ project/Renew Project/Assets/Scripts/ButtonScrips/Buttonbright.cs
@@ -1,42 +1,129 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Buttonbright : MonoBehaviour
 {
-    //Button button;
-    Image image;
+    public enum SkillType
+    {
+        RBCSpeed,
+        RBCAmount,
+        RBCHave,
+        WBCTime,
+        WBCRange,
+        WBCAmount,
+        PLTCure,
+        PLTAmount,
+        StageTime,
+        StageOx
+    }
+    public SkillType skillType;
 
+    public int myLevel;
+
+    public Image image;
+    
     bool isOn = false;
-
     void Start()
     {
-        //button = GetComponent<Button>();
         image = GetComponent<Image>();
-
-        //button.onClick.AddListener(ChangeColor);
     }
-    public void LightUp()
+    public void Bright()
     {
+        int currentLevel = GetCurrentLevel();
+        if (currentLevel != myLevel)
+        {
+            Debug.Log("èáî‘Ç™à·Ç§");
+            return;
+        }
         if (!isOn)
         {
-            //ëOÇÃLvÇâï˙ÇµÇ»Ç¢Ç∆âüÇπÇ»Ç¢ÅAåıÇÁÇ»Ç¢ÇÊÇ§Ç…Ç∑ÇÈ
-            Color c = image.color;
-
-            c.r = Mathf.Clamp01(c.r + 0.5f);
-            c.g = Mathf.Clamp01(c.g + 0.5f);
-            c.b = Mathf.Clamp01(c.b + 0.5f);
-
-            image.color = c;
+            image.color = Color.white;
             isOn = true;
+            AddLevel();
         }
     }
-    //    void ChangeColor()
-    //    {
-    //
-    //
-    //
-    //{
-    //    image.color = Color.white;
-    //}
-    //    }
+    int GetCurrentLevel()
+    {
+        switch (skillType)
+        {
+            case SkillType.RBCSpeed:
+                return SkillUnlock.rbcSpeedLevel;
+
+            case SkillType.RBCAmount:
+                return SkillUnlock.rbcAmountLevel;
+
+            case SkillType.RBCHave:
+                return SkillUnlock.rbcHaveLevel;
+
+            case SkillType.WBCTime:
+                return SkillUnlock.wbcTimeLevel;
+
+            case SkillType.WBCRange:
+                return SkillUnlock.wbcRangeLevel;
+
+            case SkillType.WBCAmount:
+                return SkillUnlock.wbcAmountLevel;
+
+            case SkillType.PLTCure:
+                return SkillUnlock.pltCureLevel;
+
+            case SkillType.PLTAmount:
+                return SkillUnlock.pltAmountLevel;
+
+            case SkillType.StageTime:
+                return SkillUnlock.stageTimeLevel;
+
+            case SkillType.StageOx:
+                return SkillUnlock.stageOxLevel;
+        }
+
+        return 0;
+    }
+
+    void AddLevel()
+    {
+        switch (skillType)
+        {
+            case SkillType.RBCSpeed:
+                SkillUnlock.rbcSpeedLevel++;
+                break;
+
+            case SkillType.RBCAmount:
+                SkillUnlock.rbcAmountLevel++;
+                break;
+
+            case SkillType.RBCHave:
+                SkillUnlock.rbcHaveLevel++;
+                break;
+
+            case SkillType.WBCTime:
+                SkillUnlock.wbcTimeLevel++;
+                break;
+
+            case SkillType.WBCRange:
+                SkillUnlock.wbcRangeLevel++;
+                break;
+
+            case SkillType.WBCAmount:
+                SkillUnlock.wbcAmountLevel++;
+                break;
+
+            case SkillType.PLTCure:
+                SkillUnlock.pltCureLevel++;
+                break;
+
+            case SkillType.PLTAmount:
+                SkillUnlock.pltAmountLevel++;
+                break;
+
+            case SkillType.StageTime:
+                SkillUnlock.stageTimeLevel++;
+                break;
+
+            case SkillType.StageOx:
+                SkillUnlock.stageOxLevel++;
+                break;
+        }
+    }
 }
